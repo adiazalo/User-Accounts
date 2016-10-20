b5d026b175f84d69c0fbe662802861ad8e0b7bc0
Alfonso <adiazalo@protonmail.com>
2016-10-20 13:18:12 -0700

    Unstyled Login Modal

---
+++ client/partials/MainNav.js
@@ -0,0 +1,8 @@
+Template.MainNav.events({
+  'click .login-toggle': ()=> {
+    Session.set('nav-toggle','open'); //setting nav-toggle to be open
+  },
+  'click .logout': ()=> {
+    Meteor.logout();
+  }
+});
